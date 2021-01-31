33f8f972dbf482295ade2dbebcdd0725a1c0c541
Ricardo Moreira <ricardo.moreira@edirectinsure.com>
2021-01-31 22:06:23 +0000

    update menu item to navigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage.component'
+import HomePage from './pages/homepage.component';
+import { BrowserRouter } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
-  return <div>
-    <HomePage />
-  </div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/hats' component={HatsPage}></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
