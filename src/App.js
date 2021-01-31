575662b5a3baedf18651f4871bc98f3962e84953
Ricardo Moreira <ricardo.moreira@edirectinsure.com>
2021-01-31 22:59:40 +0000

    added collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './pages/homepage.component';
-import { BrowserRouter } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={HomePage}></Route>
-        <Route exact path='/hats' component={HatsPage}></Route>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
